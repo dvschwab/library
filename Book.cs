550aa1451e4bcaf7ac8bd651216088262c24613f
David Schwab <dvschwab@protonmail.com>
2024-08-18 06:13:43 -0400

    Updated class definitions with access modifiers.

--- Book.cs
+++ Book.cs
@@ -1,17 +1,17 @@
 namespace LibraryApp
 {
 public interface IBook
-	Book(unsigned long ID, string title, unsigned int numberOfPages, Library library)
-	bool isAvailable(Library library)
-	Library HeldBy()
-	Patron Borrower()
+	public Book(unsigned long ID, string title, unsigned int numberOfPages, Library library)
+	public bool isAvailable(Library library)
+	public Library HeldBy()
+	public Patron Borrower()
 
 public class LibraryBook : IBook
 {
 	public long BookID { get; set; }
 	public string Title { get; set; }
 	public unsigned int NumberOfPages { get; set;}
-	LibraryBook(unsigned long ID, string title, unsigned int numberOfPages)
+	public LibraryBook(unsigned long ID, string title, unsigned int numberOfPages)
 	{
 		BookID = ID;
 		Title = title;
