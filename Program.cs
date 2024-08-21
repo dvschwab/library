e26768ef734354136604308ba25b209b7aa65591
David Schwab <dvschwab@protonmail.com>
2024-08-21 17:45:20 -0400

    Moved Library class to Program.cs (temporarily) because Program.cs couldn't find it.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace LibraryApp
 {
+    public class Library
+    {
+        public string Name {get; set;}
+        public Library(string name)
+        {
+            return false;
+        }
+    }   
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Library HomeLibrary = new Library("Burbank Rd. Library");
+            Console.WriteLine("Welcome to {0}! We have ALL THE BOOKS!", HomeLibrary.Name);
         }
     }
 }
