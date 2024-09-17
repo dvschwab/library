3e90dd70eeb4567f1a2ce5c8da2396717dd17894
David Schwab <dvschwab@protonmail.com>
2024-09-17 14:30:08 -0400

    Several changes to the classes and program. Also added a Patron class.

---
+++ Patron.cs
@@ -0,0 +1,22 @@
+namespace LibraryApp
+{
+     
+    public class Patron
+    {
+        public long CardNumber {get;set;}
+        public string PatronName {get; set;}
+        public Library HomeLibrary {get; set;}
+
+        public List<Book> Borrowed {get; set;}
+
+        public Patron( long cardNumber, string name, Library homeLibrary)
+        {
+            CardNumber = cardNumber;
+            PatronName = name;
+            HomeLibrary = homeLibrary;
+
+            this.Borrowed = new List<Book>();
+        }
+        
+    }
+}
